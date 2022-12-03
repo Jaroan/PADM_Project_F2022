69d7818604988dfdd9f6f0dca1c0665a968f9d8f
aaalissa <alissachaval@gmail.com>
2022-12-03 12:32:04 -0500

    Update motion.py

--- motion.py
+++ motion.py
@@ -17,33 +17,63 @@ from src.world import World
 
 
 #my actual imports! 
-from pybullet_tools.utils import get_joint_positions, get_link_name, get_links, get_link_pose, set_joint_positions
+from pybullet_tools.utils import get_joint_positions, get_link_name, get_links, get_link_pose, set_joint_positions, sub_inverse_kinematics, plan_cartesian_motion
 import numpy as np
 
-#get_joint_positions
+''' NOTES:
+sub_inverse_kinematics(robot, first_joint, target_link, target_pose, **kwargs)
+plan_cartesian_motion(robot, first_joint, target_link, waypoint_poses,
+                          max_iterations=200, custom_limits={}, **kwargs):
+
+
+'''
+
+#generate position based on joint limits
+def get_sample_fn(body, joints, custom_limits={}, **kwargs):
+    lower_limits, upper_limits = get_custom_limits(body, joints, custom_limits, circular_limits=CIRCULAR_LIMITS)
+    generator = interval_generator(lower_limits, upper_limits, **kwargs)
+    def fn():
+        return tuple(next(generator))
+    return fn
+
 
 #start world
 world = World(use_gui=False)
 robot = world.robot
 
 #base spawn position
-spawn_pos = np.array([.85, .8, np.pi])
-set_joint_positions(world.robot, world.base_joints, spawn_pos)
+spawn_position = np.array([.85, .8, np.pi])
+set_joint_positions(world.robot, world.base_joints, spawn_position)
 
+#print(get_joint_positions(world.robot, world.base_joints))
+#print(get_joint_positions(world.robot, world.arm_joints))
 
 #inverse kinematics help 
 tool_link = link_from_name(world.robot, 'panda_hand')
 
-test = p.calculateInverseKinematics(world.arm_joints, 6, [-0.008500000461935997, 1.2109999656677246, -0.6542999744415283])
-print(test)
+#first_joint = link_from_name(world.robot, 'x')
+print('tool link', tool_link)
+#print('test Joints', [link_from_name(world.robot, joint) for joint in world.robot])
+#first_joint = 13
 
-print(get_joint_positions(world.robot, world.base_joints))
-print(get_joint_positions(world.robot, world.arm_joints))
+first_joint = 12
+tool_link = 18
+
+target_pose = list((-0.008500000461935997, 1.2109999656677246, -0.6542999744415283)) #drawer location
+
+print('sub IK:', sub_ik_test_case := sub_inverse_kinematics(robot, first_joint, tool_link, target_pose) )
+
+#print('ik:', test := p.calculateInverseKinematics(world.robot, tool_link, target_pose))
+
+for joint in world.gripper_joints:
+    print(joint)
+
 
 
 #set_joint_positions(world.robot, world.arm_joints, test)
 
 wait_for_user()
+
 
 def get_config_from_position():
     
@@ -88,7 +118,7 @@ def motion_positions():
         
     return positions
 
-motion_positions()
+#motion_positions()
 
 '''
 obstacles = get_links(world.kitchen)
